e45bc348d5bddb5d21462960e39c3e4faf868feb
safak <safakk.bilici@gmail.com>
2021-10-30 17:07:27 +0300

    listing

--- bayesmedaug/__init__.py
+++ bayesmedaug/__init__.py
@@ -1,2 +1,27 @@
 from bayesmedaug.models.unet import VanillaUNet
 from bayesmedaug.callbacks.trainer import Trainer
+from bayesmedaug.augmentations.functional import *
+
+
+
+CURRENT_AUGMENTATIONS = [
+    Gamma,
+    GaussianBlur,
+    OpticalDistortion,
+    GaussianNoise,
+    GaussianNoiseDeterministic,
+    BrightnessContrast,
+    Rotate,
+    ShearX,
+    ShearY,
+    ShiftX,
+    ShiftY,
+    ZoomOut,
+    RandomCrop
+]
+
+
+
+def list_augmentations():
+    for aug in CURRENT_AUGMENTATIONS:
+        print(aug.__doc__)
