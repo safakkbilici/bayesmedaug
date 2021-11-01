648d0cdc4d2be9802cefe85c69c3e8a89cbfcdf0
safak <safakk.bilici@gmail.com>
2021-11-01 08:29:36 +0300

    inherited bo main class function

--- bayesmedaug/callbacks/bo.py
+++ bayesmedaug/callbacks/bo.py
@@ -1,6 +1,5 @@
 from bayes_opt import BayesianOptimization
 
-
 class BOMed(BayesianOptimization):
     def __init__(self, f, pbounds, random_state = 1):
         super(BOMed, self).__init__(f, pbounds, random_state)
